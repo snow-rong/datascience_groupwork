f4cf64e3a71492df5dd243394de2dc5d1542166b
Auraro-12123 <18813043577@163.com>
2020-07-02 13:56:41 +0800

    增加了房屋标签和建造时间

--- spider.py
+++ spider.py
@@ -1,9 +1,8 @@
 import requests
 import bs4
-import time
 import random
 import pandas as pd
-import os
+import time
 from openpyxl import load_workbook
 from fake_useragent import UserAgent
 
@@ -50,6 +49,7 @@ def get_info(url, name, writer):
     print("--------------------------------------%s-------------------------------" % name)
     house_info = []
     for i in range(1, 21):
+
         url = url + "p" + str(i) + "#filtersort"
         print("开始爬取安居客平台北京%s二手房第%s页信息....." % (name, str(i)))
         response = downloadPage(url)
@@ -62,20 +62,50 @@ def get_info(url, name, writer):
             # bs4解析文件
             titile = house.find('a').text.strip()
 
-            house_type = house.find('div', class_='details-item').span.text
-            house_area = house.find('div', class_='details-item').contents[3].text
-            house_floor = house.find('div', class_='details-item').contents[5].text
 
-            address = house.find('span', class_='comm-address').text.strip()
-            price = house.find('span', class_='price-det').text.strip()
-            unit_price = house.find('span', class_='unit-price').text.strip()
+            house_type = house.find('div', class_='details-item').span.text  #房子标题
 
+            house_area = house.find('div', class_='details-item').contents[3].text#获取面积
+
+            house_floor = house.find('div', class_='details-item').contents[5].text#获取层数
+
+            house_tags = house.find('div',class_='tags-bottom').text #获取房子标签
+            tag_length =len(house_tags)
+            if tag_length>1:
+                #标签分割
+                 tag_num=len(house.find('div',class_='tags-bottom'))
+                 if tag_num==3:
+                    tags_1 = house.find('div',class_='tags-bottom').contents[1].text
+                    tags=tags_1
+                 elif tag_num==4:
+                    tags_1 = house.find('div', class_='tags-bottom').contents[1].text
+                    tags_2 = house.find('div',class_='tags-bottom').contents[2].text
+                    tags=tags_1+' '+tags_2
+                 elif tag_num==5:
+                    tags_1 = house.find('div', class_='tags-bottom').contents[1].text
+                    tags_2 = house.find('div', class_='tags-bottom').contents[2].text
+                    tags_3=house.find('div', class_='tags-bottom').contents[3].text
+                    tags=tags_1+' '+tags_2+' '+tags_3
+            else:
+                tags='NAN'
+
+            address = house.find('span', class_='comm-address').text.strip()#获取地址
+
+            price = house.find('span', class_='price-det').text.strip()#获取房子总价
+
+            unit_price = house.find('span', class_='unit-price').text.strip()#获取每平方米的价格
+
+            length = len(house.find('div', class_='details-item'))
+            if length==9:
+                build_time=house.find('div', class_='details-item').contents[7].text   #获取建造时间
+            else:
+                build_time='NAN'
             pd1 = pd.DataFrame({'titile': titile, 'house_type': house_type, 'build_area': house_area,
                                 'bulid_floor': house_floor, 'address': address, 'price': price,
-                                'unit_price': unit_price}, index=[0])
+                                'unit_price': unit_price,'build_time':build_time,'house_tags':tags},index=[0])
             house_info.append(pd1)
 
-        second = random.randrange(3, 5)  # 每次访问间隔3到5秒
+        second = random.randrange(4, 5)  # 每次访问间隔4到5秒，防止网页要求验证
         time.sleep(second)
 
     house_info2 = pd.concat(house_info)
